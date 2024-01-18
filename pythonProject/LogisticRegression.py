b502f6dc3c8722165db1493618d183724b743641
Milos <josipovic.pr105.2019@uns.ac.rs>
2024-01-18 14:58:27 +0100

    LogisticRegression - MSE, RMSE, MAE

--- pythonProject/LogisticRegression.py
+++ pythonProject/LogisticRegression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 import csv
 
 # Read training data
@@ -16,10 +17,24 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_
 clf = LogisticRegression(C=1e5, solver='newton-cg', multi_class='multinomial')
 clf.fit(X_train, Y_train)
 
+# Predictions on the test set
 preds = clf.predict(X_test)
 
+# Calculate accuracy
 accuracy = float(np.sum(preds == Y_test)) / Y_test.shape[0]
-print("accuracy: %f" % (accuracy))
+print("Accuracy: %f" % (accuracy))
+
+# Calculate Mean Squared Error (MSE)
+mse = mean_squared_error(Y_test, preds)
+print("Mean Squared Error (MSE): %f" % mse)
+
+# Calculate Root Mean Squared Error (RMSE)
+rmse = np.sqrt(mse)
+print("Root Mean Squared Error (RMSE): %f" % rmse)
+
+# Calculate Mean Absolute Error (MAE)
+mae = mean_absolute_error(Y_test, preds)
+print("Mean Absolute Error (MAE): %f" % mae)
 
 # Read test data
 testdata = pd.read_csv('datasets/test.csv')
@@ -27,8 +42,10 @@ testdata = pd.read_csv('datasets/test.csv')
 # Exclude 'id' column from test data
 testdata = testdata.drop(columns=['id'])
 
+# Predictions on the test data
 test_prediction = clf.predict(testdata)
 
+# Write predictions to a CSV file
 file = open("Prediction.csv", "w", newline="")
 new_file = csv.writer(file)
 for i in range(len(test_prediction)):
