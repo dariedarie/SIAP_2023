ee90069a8152790733fd159e1af346004d8ed14d
elcholo01 <dvdcolak@gmail.com>
2024-02-18 02:55:58 +0100

    Commit123

---
+++ pythonProject/Common.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+def calculate_pixels(resolution):
+    try:
+        h, w = map(int, resolution.split('x'))
+        return h * w
+    except ValueError:
+        return None
+
+def data_preprocessing():
+    glavni = pd.read_csv('datasets/cleaned_all_phones.csv')
+
+    data = [glavni]
+
+    def calculate_pixels(resolution):
+        try:
+            h, w = map(int, resolution.split('x'))
+            return h * w
+        except ValueError:
+            return None
+
+    for df in data:
+        # recnik = {
+        #     'Android 8.1': 'Android 8.0',
+        #     'Android 12L': 'Android 12',
+        #     'Android 9.0 Pie': 'Android 9.0',
+        #     'Android 7.1.1': 'Android 7.0',
+        #     'Android 7.1': 'Android 7.0',
+        #     'Android 7.1.2': 'Android 7.0',
+        #     'Android 7.0.1': 'Android 7.0',
+        #     'Android 8.0 Oreo': 'Android 8.0',
+        #     'Android 8.1 Oreo': 'Android 8.0',
+        #     'Android 6': 'Android 6.0',
+        #     'Android 6.0.1': 'Android 6.0',
+        #     'Android 12 or 13': 'Android 12',
+        #     'Android 10/ Android 11': 'Android 10',
+        #     'iOS 15.4': 'iOS 15',
+        #     'iOS 14.1': 'iOS 14',
+        #     'iOS 11.1.1': 'iOS 11'
+        # }
+        # df['os'] = df['os'].replace(recnik)
+        # df[['os_name', 'os_version']] = df['os'].str.split(n=1, expand=True)
+        df['number_of_pixels'] = df['resolution'].apply(calculate_pixels)
+        # df['announcement_year'] = df.announcement_date.apply(lambda x: x.split('-')[0] if '-' in x else x.split('/')[0]).astype('int64')
+        df['announcement_year'] = df['announcement_date'].apply(lambda x: x.split('-')[0]).astype('int32')
+        # df.drop(columns=['os'], inplace=True)
+        bool_col = [col for col in df.columns if df[col].dtype == 'bool']
+        df[bool_col] = df[bool_col].astype(int)
+        outliers = df[(df['weight(g)'] > 450.0) | (df['price(USD)'] > 2000.0) | (df['ram(GB)'] > 15.0)]
+        le = LabelEncoder()
+        df['brand'] = le.fit_transform(df['brand'])
+        df['battery_type'] = le.fit_transform(df['battery_type'])
+        df['os'] = le.fit_transform(df['os'])
+        camera = [x for x in df.columns if 'video' in x]
+        df['camera_score'] = df[camera].sum(axis=1)
+        df.drop(outliers.index, inplace=True)
+        df.drop(columns=['resolution'], inplace=True)
+        df.drop(columns=['announcement_date'], inplace=True)
+        df.drop(columns=['phone_name'], inplace=True)
+        df.drop(bool_col, axis=1, inplace=True)
+
+
+    # Separate features and target variable
+    X = glavni.drop(['price(USD)'], axis=1)
+    y = glavni['price(USD)']
+
+    # Split the data into training and testing sets
+    xtrain, xtest, ytrain, ytest = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    return xtrain,ytrain,xtest,ytest
--- pythonProject/RandomForestRegression.py
+++ pythonProject/RandomForestRegression.py
@@ -7,19 +7,11 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-from XGBRegression import data_preprocessing
-
+from Common import data_preprocessing
 
 xtrain,ytrain,xtest,ytest = data_preprocessing()
-print(ytrain.head())
 
-# print(xtrain.isna().sum())
-# nan_rows = xtrain[xtrain.isna().any(axis=1)]
-# print(nan_rows)
-
-print(len(xtrain), len(ytrain))
-rf = RandomForestRegressor(random_state=15)
-
+rf = RandomForestRegressor(random_state=42)
 rf.fit(xtrain, ytrain)
 
 rf_pred = rf.predict(xtest)
@@ -40,18 +32,3 @@ print(f"Mean Squared Error: {mse}")
 
 rmse = np.sqrt(mse)
 print(f"Root Mean Squared Error: {rmse}")
-
-print(f"Mean of 'price(USD)': {train_data['price(USD)'].mean()}")
-
-#For testing only!!!!
-# Create a dataframe with feature impact
-# df_impact = pd.DataFrame({'feature': X.columns, 'impact': rf.feature_importances_})
-# df_impact.sort_values(by='impact', ascending=False, inplace=True)
-# print(df_impact)
-
-pred_df = pd.DataFrame({
-    'actual price': ytest,
-    'random forest': rf_pred
-})
-
-print(pred_df.head(20))
