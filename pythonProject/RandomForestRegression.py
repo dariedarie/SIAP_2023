71859cfc5ed83ce9480a2b0e2b289b90d40b9bf5
elcholo01 <dvdcolak@gmail.com>
2024-03-19 21:44:20 +0100

    RadnomF hyperparameters

--- pythonProject/RandomForestRegression.py
+++ pythonProject/RandomForestRegression.py
@@ -15,10 +15,28 @@ import matplotlib.pyplot as plt
 
 xtrain,ytrain,xtest,ytest = data_preprocessing()
 
-rf = RandomForestRegressor(random_state=42)
-rf.fit(xtrain, ytrain)
+param_grid = {
+    'n_estimators': [10, 50, 100],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
 
-rf_pred = rf.predict(xtest)
+rf = RandomForestRegressor(random_state=42)
+
+scorer = make_scorer(r2_score)
+
+grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, scoring=scorer, cv=5)
+
+grid_search.fit(xtrain, ytrain)
+
+print("Best hyperparameters:", grid_search.best_params_)
+
+best_rf_model = grid_search.best_estimator_
+
+#rf.fit(xtrain, ytrain)
+
+rf_pred = best_rf_model.predict(xtest)
 r2 = r2_score(ytest, rf_pred)
 print(f"R-squared (R2): {r2}")
 n = xtest.shape[0]
@@ -26,18 +44,18 @@ k = xtest.shape[1]
 adjusted_r2 = 1 - ((1 - r2) * (n - 1) / (n - k - 1))
 print(f"Adjusted R-squared: {adjusted_r2}")
 
-rf_pred = rf.predict(xtest)
+rf_pred = best_rf_model.predict(xtest)
 mae = mean_absolute_error(ytest, rf_pred)
 print(f"Mean Absolute Error: {mae}")
 
-rf_pred = rf.predict(xtest)
+rf_pred = best_rf_model.predict(xtest)
 mse = mean_squared_error(ytest, rf_pred)
 print(f"Mean Squared Error: {mse}")
 
 rmse = np.sqrt(mse)
 print(f"Root Mean Squared Error: {rmse}")
 
-df_impact = pd.DataFrame({'feature': xtrain.columns, 'impact': rf.feature_importances_})
+df_impact = pd.DataFrame({'feature': xtrain.columns, 'impact': best_rf_model.feature_importances_})
 df_impact.sort_values(by='impact', ascending=False, inplace=True)
 print(df_impact)
 
@@ -83,4 +101,3 @@ plt.xlabel('Predviđene cene')
 plt.ylabel('Ostaci')
 plt.axhline(y=0, color='r', linestyle='--')
 plt.show()
-
